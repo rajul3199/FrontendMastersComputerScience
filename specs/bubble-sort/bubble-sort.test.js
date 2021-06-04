42d141f01278d2b20e9745a8df9fc36888605f55
Rajul Vishwakarma <0206cs161120@ggits.net>
2021-06-04 12:11:16 +0530

    Factorial Problem Solved

--- specs/bubble-sort/bubble-sort.test.js
+++ specs/bubble-sort/bubble-sort.test.js
@@ -11,25 +11,26 @@
 */
 
 function bubbleSort(nums) {
-  // code goes here
   let swapped = false;
   do {
     swapped = false;
-    for(let i=0;i<nums.length;i++){
-      if(nums[i] >= nums[i+1]){
-        const temp =  nums[i];
-        nums[i] = nums[i+1];
-        nums[i] = temp;
+    for (let i = 0; i < nums.length; i++) {
+      // snapshot(nums);
+      if (nums[i] > nums[i + 1]) {
+        const temp = nums[i];
+        nums[i] = nums[i + 1];
+        nums[i + 1] = temp;
+        swapped = true;
       }
     }
-    return nums;
-  }
+  } while (swapped);
+  // snapshot(nums);
 }
 
 // unit tests
 // do not modify the below code
 test("bubble sort", function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
-  const sortedNums = bubbleSort(nums);
-  expect(sortedNums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+  bubbleSort(nums);
+  expect(nums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 });
